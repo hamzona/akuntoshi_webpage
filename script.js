e02b4d9e07696c405918b35ea82c035d91e4f76c
hamzona <hamzadjedovic2000@gmail.com>
2026-03-15 13:37:45 +0100

    network animation

--- script.js
+++
@@ -1,9 +0,0 @@
-let laptop = document.querySelector(".laptop");
-let range = document.querySelector(".range");
-let screen = document.querySelector(".screen");
-
-range.addEventListener("change", () => {
-  var rangeValue = range.value;
-
-  screen.style.transform = `rotateX(${rangeValue}deg)`;
-});
